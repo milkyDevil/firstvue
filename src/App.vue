ee7f9e215416307b4d3fd99db2a4c4264e320c57
milkyDevil <begum.mahmuda16@gmail.com>
2021-02-02 17:06:13 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,111 @@
+<template>
+  <v-app id="inspire">
+     <v-navigation-drawer
+     v-model="drawer"
+     :mobile-breakpoint="768"
+     app
+     >
+     <v-img
+     class="pa-4 pt-7"
+     src="mountains.jpg"
+      height="170"
+      gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+    >
+     <v-avatar size="70" class="mb-2">
+      <img
+        src="testimonial-4-80x80.jpg"
+        alt="Milky"
+      >
+    </v-avatar>
+    <div class="white--text text-subtitle-1 font-weight-bold">Milky Khan</div>
+    <div class="white--text text-subtitle-2">milky_khan</div>
+    </v-img>
+      <!--<v-divider></v-divider>-->
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          :to="item.to"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+<v-app-bar
+      app
+      color="primary"
+      dark
+      src="mountains.jpg"
+      prominent
+      :height="$route.path === '/' ? '230' : '170'"
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(19,84,122,.9), rgba(128,208,199,.9)"
+        >
+        </v-img>
+      </template>
+      <v-container class="pa-0">
+        <v-row>
+          <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-spacer></v-spacer>
+        <search v-if="$route.path === '/'"/>
+        </v-row>
+        <v-row>
+         <v-toolbar-title class="text-h4 ml-4">
+           {{ $store.state.appTitle }}
+        </v-toolbar-title>  
+        </v-row>
+        <v-row>
+          <live-date-time></live-date-time>
+        </v-row>
+        <v-row v-if="$route.path === '/'">
+          <field-add-tasks />
+        </v-row>
+      </v-container> 
+    </v-app-bar>
+
+    <v-main>
+     <router-view></router-view>
+     <snackbar />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({ 
+      drawer: null,
+      items: [
+          { title: 'Todo', icon: 'mdi-format-list-checks', to: '/' },
+          { title: 'About', icon: 'mdi-information-variant', to: '/about' },
+        ],
+      }),
+      mounted(){
+        this.$store.dispatch('getTasks')
+      },
+      components: {
+        'search' : require('@/components/Tools/Search.vue').default,
+        'live-date-time' : require('@/components/Tools/LiveDateTime.vue').default,
+        'field-add-tasks': require('@/components/Todo/FieldAddTask.vue').default,
+        'snackbar' : require('@/components/Shared/Snackbar.vue').default,
+      }
+  }
+</script>
+<style lang="sass">
+  .header-container
+    max-width: none !important
+</style>
---
+++ src/views/Todo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    
+    <list-tasks 
+    v-if="$store.state.tasks.length"
+    />
+   <no-tasks
+    v-else
+   />
+    <button-done-sorting
+    v-if="$store.state.sorting"
+    >
+
+    </button-done-sorting>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components:{
+    'list-tasks': require('@/components/Todo/ListTasks.vue').default,
+    'no-tasks': require('@/components/Todo/NoTask.vue').default,
+    'button-done-sorting': require('@/components/Todo/ButtonDoneSorting.vue').default,
+  }
+}
+</script>
